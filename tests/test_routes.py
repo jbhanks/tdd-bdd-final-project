0c6d02fccaa9db5437860119fb808e0c73ec344c
jbhanks <theia@theiadocker-jhanks1981>
2024-11-01 02:42:21 -0400

    started test_get_product

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,6 +166,18 @@ class TestProductRoutes(TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print("test_product.id", test_product.id)
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
 
     ######################################################################
     # Utility functions
