cbeaafd52e5ffc95b5ea7386a1c0b880512460c3
jbhanks <theia@theiadocker-jhanks1981>
2024-11-01 03:21:45 -0400

    added 'read a product' to routes

--- service/routes.py
+++ service/routes.py
@@ -109,7 +109,15 @@ def create_products():
 #
 # PLACE YOUR CODE HERE TO READ A PRODUCT
 #
-
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_one_product(product_id):
+    """Gets a single product by id"""
+    app.logger.info("Request to Retrieve a product with id [%s]", product_id)
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id {product_id}' was not found.")
+    app.logger.info("Returning product: %s", product.name)
+    return product.serialize(), status.HTTP_200_OK
 ######################################################################
 # U P D A T E   A   P R O D U C T
 ######################################################################
